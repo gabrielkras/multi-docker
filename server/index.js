f282962940e53e88bfcfc5f105c2123827f81011
Gabriel Sousa Kraszczuk <gabrielkras@hotmail.com>
2018-10-08 10:51:44 -0300

    Configuração Docker-Compose

--- server/index.js
+++ server/index.js
@@ -51,6 +51,9 @@ app.get('/values/current', async (req, res) => {
 
 app.post('/values', async (req, res) => {
     const index = req.body.index;
+    if (!index) {
+        return res.status(400).send("Invalid Index");
+    }
     if(parseInt(index) > 40) {
         return res.status(422).send('Index too high');
     }
